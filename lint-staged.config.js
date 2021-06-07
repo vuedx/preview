728f2b4720daaec839d812b67b8e4cbb64b8f2ff
Rahul Kadyan <hi@znck.me>
2021-06-07 08:53:50 -0700

    add workflow source

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  '**/*.{ts,mjs,cjs,js,vue}': ['eslint --quiet --fix --cache', 'prettier --write'],
+  '*.{css,md}': ['prettier --write'],
+  '**/*.{json,graphql}': ['prettier --write'],
+  '.github/**/*.{yaml,yml}': [
+    'node scripts/prepare-workflows.mjs',
+    'git add .github/workflows',
+    'prettier --write',
+  ],
+};
