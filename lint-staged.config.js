4cda60e0dda6045aa62b5283d24b6be18436e229
Rahul Kadyan <hi@znck.me>
2021-06-07 10:15:59 -0700

    release: packages using CI

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '**/*.{ts,mjs,cjs,js,vue}': ['eslint --quiet --fix --cache', 'prettier --write'],
   '*.{css,md}': ['prettier --write'],
   '**/*.{json,graphql}': ['prettier --write'],
-  '.github/**/*.{yaml,yml}': [
+  '.github/**/*.yaml': [
     'node scripts/prepare-workflows.mjs',
     'git add .github/workflows',
     'prettier --write',
