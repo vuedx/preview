d47255746d42a5a423037718ef8d8b62b5a692f0
Rahul Kadyan <hi@znck.me>
2022-02-27 21:34:03 -0800

    fix pre-release script

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '**/*.{ts,mjs,cjs,js,vue}': ['eslint --quiet --fix --cache', 'prettier --write'],
   '*.{css,md}': ['prettier --write'],
   '**/*.{json,graphql}': ['prettier --write'],
-  '.github/workflows/**/*.yaml': [
+  '.github/**/*.{yaml,yml}': [
     'node scripts/prepare-workflows.mjs',
     'git add .github/workflows',
     'prettier --write',
